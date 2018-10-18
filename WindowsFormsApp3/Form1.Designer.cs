524083342ab5c2b9693fced13d9fddb4cf271699
BinLive <binbox@inbox.ru>
2018-10-18 22:40:32 +0300

    need

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp3
 {
-    partial class Form1
+    partial class Helpdesk
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(274, 75);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(405, 400);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Статус ККМ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP или имя ПК:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Статус обмена ОФД";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Заводской номер ККМ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Версия прошивки ККМ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(42, 356);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Статус ТБО";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(718, 75);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Установка службы GoRPS";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(718, 104);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(186, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Остановить службу";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(718, 133);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(186, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Запуск службы";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Helpdesk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(927, 633);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Helpdesk";
+            this.Text = "Helpdesk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -7,14 +7,185 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
 
 namespace WindowsFormsApp3
 {
-    public partial class Form1 : Form
+    public partial class Helpdesk : Form
     {
-        public Form1()
+        public Helpdesk()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            string url = "http://" + textBox1.Text + ":9000/api/kkm/status";
+            var webClient = new WebClient();
+            {
+                string response = webClient.DownloadString(url);
+                byte[] bytes = Encoding.Default.GetBytes(response);
+                string newString = Encoding.UTF8.GetString(bytes);
+                textBox2.Text = newString;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = "http://" + textBox1.Text + ":9000/api/kkm/unsent-docs";
+            var webClient = new WebClient();
+            {
+                string response = webClient.DownloadString(url);
+                byte[] bytes = Encoding.Default.GetBytes(response);
+                string newString = Encoding.UTF8.GetString(bytes);
+                textBox2.Text = newString;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = "http://" + textBox1.Text + ":9000/api/kkm/factory-number";
+            var webClient = new WebClient();
+            {
+                string response = webClient.DownloadString(url);
+                byte[] bytes = Encoding.Default.GetBytes(response);
+                string newString = Encoding.UTF8.GetString(bytes);
+                textBox2.Text = newString;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string url = "http://" + textBox1.Text + ":9000/api/kkm/version";
+            var webClient = new WebClient();
+            {
+                string response = webClient.DownloadString(url);
+                byte[] bytes = Encoding.Default.GetBytes(response);
+                string newString = Encoding.UTF8.GetString(bytes);
+                textBox2.Text = newString;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string url = "http://" + textBox1.Text + ":9000/api/pinpad/status";
+            var webClient = new WebClient();
+
+            {
+                string response = webClient.DownloadString(url);
+                byte[] bytes = Encoding.Default.GetBytes(response);
+                string newString = Encoding.UTF8.GetString(bytes);
+                textBox2.Text = newString;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Копируем файлы..";
+            try
+            { 
+                string fileName = "installer.exe";
+                string sourcePath = @"C:\helpdesk\";
+                string targetPath = @"\\" + textBox1.Text + @"\C$\install\";
+
+                // Use Path class to manipulate file and directory paths.
+                string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+                string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+                // To copy a folder's contents to a new location:
+                // Create a new target folder, if necessary.
+                if (!System.IO.Directory.Exists(targetPath))
+                {
+                System.IO.Directory.CreateDirectory(targetPath);
+                }
+
+                // To copy a file to another location and 
+                // overwrite the destination file if it already exists.
+                System.IO.File.Copy(sourceFile, destFile, true);
+
+                // To copy all the files in one directory to another directory.
+                // Get the files in the source folder. (To recursively iterate through
+                // all subfolders under the current directory, see
+                // "How to: Iterate Through a Directory Tree.")
+                // Note: Check for target path was performed previously
+                //       in this code example.
+                if (System.IO.Directory.Exists(sourcePath))
+                     {
+                     string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                    // Copy the files and overwrite destination files if they already exist.
+                     foreach (string s in files)
+                     {
+                     // Use static Path methods to extract only the file name from the path.
+                     fileName = System.IO.Path.GetFileName(s);
+                     destFile = System.IO.Path.Combine(targetPath, fileName);
+                     System.IO.File.Copy(s, destFile, true);
+                     }
+                     textBox2.Text = "Скопирован installer.exe!";
+                }
+                else
+                {
+                textBox2.Text = "Source path does not exist!";
+                }
+            }
+                catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка загрузки файла!");
+                return;
+            }
+            
+            
+           
+                Process process = new Process();
+                // Configure the process using the StartInfo properties.
+                process.StartInfo.FileName = "c:\\helpdesk\\psexec.exe";
+                process.StartInfo.Arguments = "-s \\\\" + textBox1.Text + " c:\\install\\installer.exe";
+           //     process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+           //     process.WaitForExit();
+                textBox2.Text = "Проверяйте!";
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c sc \\\\" + textBox1.Text + " stop wildberries_GoRPS";
+                 process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            //     process.WaitForExit();
+            textBox2.Text = "Остановлена!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка загрузки файла!");
+                return;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = new Process();
+                // Configure the process using the StartInfo properties.
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c sc \\\\" + textBox1.Text + " start wildberries_GoRPS";
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                //     process.WaitForExit();
+                textBox2.Text = "Запущена!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка загрузки файла!");
+                return;
+            }
         }
     }
 }
